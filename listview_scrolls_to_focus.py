dd591a0df13b067d0fdb5eb6fd3546ab24213431
David Fokkema <davidfokkema@icloud.com>
2023-11-12 19:59:17 +0100

    Added banner and don't scroll listview, but whole screen

--- listview_scrolls_to_focus.py
+++ listview_scrolls_to_focus.py
@@ -4,12 +4,23 @@ from textual.widgets import ListItem, ListView, Static
 
 class ListViewScrollApp(App[None]):
     CSS = """
+        #banner {
+            text-align: center;
+            text-style: bold;
+            padding: 1 2;
+        }
+
+        ListView {
+            height: auto;
+        }
+
         ListItem {
             padding: 1 2;
         }
     """
 
     def compose(self) -> ComposeResult:
+        yield Static("ACME Scrolling Test!", id="banner")
         items = [ListItem(Static(f"Item {i}")) for i in range(1, 21)]
         yield ListView(*items)
 
