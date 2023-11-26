5a58394cb60aa6bba84c346a5b0f4a88cad438c0
David Fokkema <davidfokkema@icloud.com>
2023-11-26 17:47:40 +0100

    Update listview_scrolls_to_focus.py

--- listview_scrolls_to_focus.py
+++ listview_scrolls_to_focus.py
@@ -3,6 +3,7 @@ from textual.widgets import ListItem, ListView, Static
 
 
 class ListViewScrollApp(App[None]):
+    BINDINGS = [("h", "scroll()", "Home")]
     CSS = """
         #banner {
             text-align: center;
@@ -23,6 +24,16 @@ class ListViewScrollApp(App[None]):
         yield Static("ACME Scrolling Test!", id="banner")
         items = [ListItem(Static(f"Item {i}")) for i in range(1, 21)]
         yield ListView(*items)
+
+    def on_mount(self) -> None:
+        self.screen.scroll_to_widget(self.query_one("#banner"))
+
+    def action_scroll(self) -> None:
+        # self.screen.scroll_home()
+        self.screen.scroll_to_widget(self.query_one("#banner"))
+
+    def on_list_view_highlighted(self, event: ListView.Highlighted) -> None:
+        self.screen.scroll_to_widget(event.item)
 
 
 if __name__ == "__main__":
