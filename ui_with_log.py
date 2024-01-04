bf8db2a41b89bbc9c9789f1a344be27d997abfd4
David Fokkema <davidfokkema@icloud.com>
2024-01-04 22:24:34 +0100

    Simple UI with buttons and RichLog

---
+++ ui_with_log.py
@@ -0,0 +1,37 @@
+from textual import on
+from textual.app import App, ComposeResult
+from textual.containers import Center, Horizontal
+from textual.widgets import Button, RichLog
+
+
+class LoggingApp(App[None]):
+    CSS = """
+        Horizontal {
+            height: auto;
+            align: center top;
+        }
+
+        Button {
+            margin: 1 2;
+        }
+
+        RichLog {
+            padding: 1 2;
+        }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            yield Button("Green", id="green", variant="primary")
+            yield Button("Red", id="red")
+            yield Button("Blue", id="blue")
+        yield RichLog(markup=True)
+
+    @on(Button.Pressed)
+    def log_text(self, event: Button.Pressed) -> None:
+        color = event.button.id
+        self.query_one("RichLog").write(f"Hi, I'm [bold {color}]{color}![/] :wink:")
+
+
+if __name__ == "__main__":
+    LoggingApp().run()
