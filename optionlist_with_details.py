9fad29f2dd207da8ed11e6c92ac5b40749ce8465
David Fokkema <davidfokkema@icloud.com>
2024-05-22 16:22:19 +0200

    Small UI tweaks

--- optionlist_with_details.py
+++ optionlist_with_details.py
@@ -1,26 +1,29 @@
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal, ScrollableContainer
+from textual.containers import Horizontal, VerticalScroll
 from textual.widgets import Label, OptionList
 
-ITEMS = ["str", "int", "float"]
+ITEMS = ["str", "int", "float", "list", "dict", "set"]
 
 
 class ShowDetailsApp(App[None]):
     CSS = """
         OptionList {
-            width: 1fr;
+            width: 15;
             height: 1fr;
         }
         VerticalScroll {
             width: 1fr;
             height: 1fr;
         }
+        Label {
+            width: 100%;
+        }
     """
 
     def compose(self) -> ComposeResult:
         with Horizontal():
             yield OptionList(*ITEMS)
-            with ScrollableContainer():
+            with VerticalScroll():
                 yield Label(id="details")
 
     def on_option_list_option_highlighted(
