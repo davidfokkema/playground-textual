e1c760814a742c88efe622550b36f4bcfa382139
David Fokkema <davidfokkema@icloud.com>
2024-06-27 21:24:50 +0200

    Fancy heartbeat

--- heartbeat_spinner.py
+++ heartbeat_spinner.py
@@ -1,6 +1,7 @@
-import itertools
+import math
 
 from textual.app import App, ComposeResult
+from textual.color import Color
 from textual.widgets import Label
 
 
@@ -11,15 +12,19 @@ class HeartbeatLabel(Label):
         }
     """
 
-    COLORS = itertools.cycle(["red", "blue", "green"])
+    t = 0
 
     def on_mount(self) -> None:
-        self.set_interval(interval=0.5, callback=self.blink)
+        self.set_interval(interval=1 / 30, callback=self.modulate_color)
         # start with a color
-        self.blink()
+        self.modulate_color()
 
-    def blink(self) -> None:
-        self.styles.background = next(self.COLORS)
+    def modulate_color(self) -> None:
+        # green, fully saturated, lightness varying smoothly between 0.1 and 0.6
+        self.styles.background = Color.from_hsl(
+            h=120 / 360, s=1.0, l=0.1 + 0.25 * (1 + math.cos(self.t))
+        )
+        self.t += math.pi / 30
 
 
 class MyApp(App[None]):
